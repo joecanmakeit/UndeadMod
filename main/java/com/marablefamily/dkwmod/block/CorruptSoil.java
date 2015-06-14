9b242cf0c83c3d0efd6876cf47a0b0f6b52001c8
Joe Allington <joecanmakeit@gmail.com>
2015-06-13 20:24:46 -0700

    What's that ticking?

--- main/java/com/marablefamily/dkwmod/block/CorruptSoil.java
+++ main/java/com/marablefamily/dkwmod/block/CorruptSoil.java
@@ -19,6 +19,7 @@ public class CorruptSoil extends Block {
 	private int timer = 1;
 	private boolean currentlySpreading = false;
 	private int direction = 0;
+	private int debugCount = 0;
 	private static int dirX(int dir) {
 		switch(dir) {
 		case 0:  return -1;
@@ -52,7 +53,7 @@ public class CorruptSoil extends Block {
 		this.setBlockTextureName("dkwmod:corruptsoil");
 		this.setCreativeTab(CreativeTabs.tabBlock);
 		this.setHardness(0.8F);
-		this.setTickRandomly(true);
+		this.setTickRandomly(false);
 	}
 	
 	/**
@@ -74,7 +75,7 @@ public class CorruptSoil extends Block {
 		--this.timer;
     	if(this.timer > 0)
     		return;
-    	
+    	    	
     	int nx = x + dirX(this.direction);
     	int ny = y + dirY(this.direction);
     	int nz = z + dirZ(this.direction);
@@ -85,68 +86,8 @@ public class CorruptSoil extends Block {
     			world.setBlock(x, y, z, Blocks.air, 0, 2);
     	}
     	
-    	this.timer = 20;
+    	this.timer = 20000;
     	this.direction = (this.direction+1) % 6;
     	this.currentlySpreading = !this.currentlySpreading;
-    	
-    	if(true) return;
-    	
-    	if (r.nextDouble() > 0.8) {
-	    	double corruptionPulse = Math.sin((double)world.getWorldTime() * 0.001) * 0.5 + 0.5;
-	    	
-	    	if (r.nextDouble() > corruptionPulse) {
-	    		if (r.nextDouble() < expansionChance[countAdjacentBlocks(world, x, y, z, Blocks.air)]) {
-	    			convertAdjacentBlock(world, x, y, z, r);
-	    		}
-	    	} else {
-	    		if (r.nextDouble() < reductionChance[countAdjacentBlocks(world,x,y,z,instance)]) {
-	    			world.setBlock(x,y,z,Blocks.air);
-	    		}
-	    	}
-    	}
-    }
-    
-    private int countAdjacentBlocks(World world, int x, int y, int z, Block blockType) {
-    	int count = 0;
-    	
-    	for (int i = 0; i < 6; ++i) {
-    		if (getNeighbor(world,x,y,z,i) == blockType) ++count;
-    	}
-    	
-    	return count;
-    }
-    
-    private void convertAdjacentBlock(World world, int x, int y, int z, Random r) {
-    	ArrayList<Integer> validDirections = new ArrayList<Integer>();
-
-    	for (int i = 0; i < 6; ++i) {
-    		if (getNeighbor(world,x,y,z,i) == Blocks.air) validDirections.add(i);
-    	}
-    	
-    	int d = r.nextInt(validDirections.size());
-    	setNeighbor(world,x,y,z,d,instance);
-    }
-    
-    private Block getNeighbor(World world, int x, int y, int z, int dir) {
-    	switch(dir) {
-    	case 0: return world.getBlock(x+1,y,z);
-    	case 1: return world.getBlock(x-1,y,z);
-    	case 2: return world.getBlock(x,y+1,z);
-    	case 3: return world.getBlock(x,y-1,z);
-    	case 4: return world.getBlock(x,y,z+1);
-    	case 5: return world.getBlock(x,y,z-1);
-    	default: return null;
-    	}
-    }
-    
-    private void setNeighbor(World world, int x, int y, int z, int dir, Block block) {
-    	switch(dir) {
-    	case 0: world.setBlock(x+1,y,z,block);
-    	case 1: world.setBlock(x-1,y,z,block);
-    	case 2: world.setBlock(x,y+1,z,block);
-    	case 3: world.setBlock(x,y-1,z,block);
-    	case 4: world.setBlock(x,y,z+1,block);
-    	case 5: world.setBlock(x,y,z-1,block);
-    	}
     }
 }
