56fdf1ccf06738a0536e044276bfa3c5f762e033
Tommy <tsbandit275@gmail.com>
2015-06-14 13:40:54 -0700

    refactored player tick code, added nearby-block-counter

--- main/java/com/marablefamily/dkwmod/DKWFMLEvents.java
+++ main/java/com/marablefamily/dkwmod/DKWFMLEvents.java
@@ -13,6 +13,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
@@ -30,42 +31,75 @@ public class DKWFMLEvents {
 	int timeToMax = 140;
 	
 	Random random = new Random();
-	HashMap<String, Integer> playerStandingTimes = new HashMap<String, Integer>();
 	
-	/*
+	class ExtraPlayerState {
+		public int standingTime;
+		public int blocksNearby;
+	}
+	
+	HashMap<String, ExtraPlayerState> extraPlayerState = new HashMap<String, ExtraPlayerState>();
+	
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent e) {
-		
 		if (!e.player.worldObj.isRemote && e.phase == e.phase.END) {
 			String name = e.player.getCommandSenderName();
-			if (!playerStandingTimes.containsKey(name)) {
-				playerStandingTimes.put(name, 0);
+
+			if (!extraPlayerState.containsKey(name)) {
+				extraPlayerState.put(name, new ExtraPlayerState());
 			}
-			if (Math.abs(e.player.motionX) < 0.1 && Math.abs(e.player.motionY) < 0.1 && Math.abs(e.player.motionZ) < 0.1) {
-				playerStandingTimes.put(name, playerStandingTimes.get(name)+1);
-			}
-			else {
-				playerStandingTimes.put(name, 0);
-			}
-			ItemStack boots = e.player.getCurrentArmor(0);
 			
-			// TEMP FIX -- Change boots to Frost Boots!
-			if (boots != null && boots.getItem() == Items.chainmail_boots) {
-				int time = playerStandingTimes.get(name);
-				int rad = minRad + ((maxRad-minRad)*(Math.min(timeToMax, time)/timeToMax));
-				int x = (int)(e.player.posX);
-				int y = (int)(e.player.posY - 1);
-				int z = (int)(e.player.posZ);
-				e.player.worldObj.setBlock(x,y,z,Blocks.ice,0,2);
-				int i = (int) (Math.random()*2*rad - rad);
-				int k = (int) (Math.random()*2*rad - rad);
-				while (Math.sqrt(i*i + k*k) > rad) {
-					i = (int) (Math.random()*2*rad - rad);
-					k = (int) (Math.random()*2*rad - rad);
-				}
-				e.player.worldObj.setBlock(x+i, y, z+k, Blocks.ice, 0, 2);
-			}
+			ExtraPlayerState extra = extraPlayerState.get(name);
+			
+			updateBlocksNearby(e, extra);
+			
+//			updateFrostBoots(e, extra);
 		}
 	}
-	*/
+
+	private void updateBlocksNearby(PlayerTickEvent e, ExtraPlayerState s) {
+		
+		// Approximately 12.5% of nearby blocks should be non-air.
+		// If fewer, then the count will drift to zero.
+		// If more, then the count will drift to 100.
+		
+		World w = e.player.worldObj;
+		
+		int x = random.nextInt(9) - 4 + (int) e.player.posX;
+		int y = random.nextInt(9) - 4 + (int) e.player.posY;
+		int z = random.nextInt(9) - 4 + (int) e.player.posZ;
+		if(w.getBlock(x, y, z) == Blocks.air)
+			--s.blocksNearby;
+		else
+			s.blocksNearby += 7;
+		
+		if(random.nextInt(40) == 0)
+			System.out.println(s.blocksNearby);
+	}
+	
+	private void updateFrostBoots(PlayerTickEvent e, ExtraPlayerState extra) {
+
+		if (Math.abs(e.player.motionX) < 0.1 && Math.abs(e.player.motionY) < 0.1 && Math.abs(e.player.motionZ) < 0.1)
+			++extra.standingTime;
+		else
+			extra.standingTime = 0;
+		
+		ItemStack boots = e.player.getCurrentArmor(0);
+		// TEMP FIX -- Change boots to Frost Boots!
+		if (boots != null && boots.getItem() == Items.chainmail_boots) {
+			int time = extra.standingTime;
+			int rad = minRad + ((maxRad-minRad)*(Math.min(timeToMax, time)/timeToMax));
+			int x = (int)(e.player.posX);
+			int y = (int)(e.player.posY - 1);
+			int z = (int)(e.player.posZ);
+			e.player.worldObj.setBlock(x,y,z,Blocks.ice,0,2);
+			int i = (int) (Math.random()*2*rad - rad);
+			int k = (int) (Math.random()*2*rad - rad);
+			while (Math.sqrt(i*i + k*k) > rad) {
+				i = (int) (Math.random()*2*rad - rad);
+				k = (int) (Math.random()*2*rad - rad);
+			}
+			e.player.worldObj.setBlock(x+i, y, z+k, Blocks.ice, 0, 2);
+		}
+
+	}
 }
