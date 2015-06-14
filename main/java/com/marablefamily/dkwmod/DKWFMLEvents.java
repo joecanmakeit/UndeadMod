d71bee0c535977a35551869e941d5eb0a27f1e40
EliMarable <edm1234@sbcglobal.net>
2015-06-14 13:30:24 -0700

    Initial attempt at death > change dimensions code

--- main/java/com/marablefamily/dkwmod/DKWFMLEvents.java
+++ main/java/com/marablefamily/dkwmod/DKWFMLEvents.java
@@ -7,13 +7,20 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
@@ -31,6 +38,71 @@ public class DKWFMLEvents {
 	
 	Random random = new Random();
 	HashMap<String, Integer> playerStandingTimes = new HashMap<String, Integer>();
+	
+	@SubscribeEvent
+	public void onPlayerRespawn(PlayerRespawnEvent e) {		
+		if (e.player.dimension != -2) {
+			e.player.travelToDimension(-2);
+			
+			
+		}
+	}
+	
+	@SubscribeEvent
+	public void onWorldLoad(WorldEvent e) {
+		
+	}
+	
+	private void sendToDimension(Entity e, int dimension, Teleporter teleporter) {
+		World w = e.worldObj;
+		
+		if (!w.isRemote && !e.isDead)
+        {
+            w.theProfiler.startSection("changeDimension");
+            // Get the current dimension world server
+            MinecraftServer minecraftserver = MinecraftServer.getServer();
+            int currentDimension = e.dimension;
+            WorldServer currentWorldServer = minecraftserver.worldServerForDimension(currentDimension);
+           
+            // Get the target dimension world server
+            WorldServer finalWorldServer;
+            if (currentDimension == dimension) {
+            	finalWorldServer = minecraftserver.worldServerForDimension(0);
+                e.dimension = 0;
+            } else {
+            	 finalWorldServer = minecraftserver.worldServerForDimension(dimension);
+                 e.dimension = dimension;
+            }
+
+            // Remove this entity from the current dimension
+            w.removeEntity(e);
+            e.isDead = false;
+            w.theProfiler.startSection("reposition");
+            minecraftserver.getConfigurationManager().transferEntityToWorld(e, currentDimension, currentWorldServer, finalWorldServer, teleporter);
+            w.theProfiler.endStartSection("reloading");
+            Entity newEntity = EntityList.createEntityByName(EntityList.getEntityString(e), finalWorldServer);
+
+            if (newEntity != null)
+            {
+            	newEntity.copyDataFrom(e, true);
+
+                if (currentDimension == dimension)
+                {
+                    ChunkCoordinates chunkcoordinates = finalWorldServer.getSpawnPoint();
+                    chunkcoordinates.posY = w.getTopSolidOrLiquidBlock(chunkcoordinates.posX, chunkcoordinates.posZ);
+                    newEntity.setLocationAndAngles((double)chunkcoordinates.posX, (double)chunkcoordinates.posY, (double)chunkcoordinates.posZ, newEntity.rotationYaw, newEntity.rotationPitch);
+                }
+
+                finalWorldServer.spawnEntityInWorld(newEntity);
+            }
+
+            e.isDead = true;
+            w.theProfiler.endSection();
+            currentWorldServer.resetUpdateEntityTick();
+            finalWorldServer.resetUpdateEntityTick();
+            w.theProfiler.endSection();
+        }
+	}
 	
 	/*
 	@SubscribeEvent
