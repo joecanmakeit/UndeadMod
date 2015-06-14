2200eaca51d0babe054d56e6d0bbf44f53d40415
Joe Allington <joecanmakeit@gmail.com>
2015-06-14 13:38:25 -0700

    Soul floating fixed

--- main/java/com/marablefamily/dkwmod/mobs/EntitySoul.java
+++ main/java/com/marablefamily/dkwmod/mobs/EntitySoul.java
@@ -5,12 +5,15 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 public class EntitySoul extends EntityMob {
 	
 	public static double sightRange = 16.0;
-
+    private boolean rising = true;
+    private int risingTimer = 50;
+    
 	public EntitySoul(World w) {
 		super(w);
 		this.setSize(0.5F, 1.0F);
@@ -32,4 +35,34 @@ public class EntitySoul extends EntityMob {
     {
         return Items.bone;
     }
+    
+    protected void fall(float p_70069_1_) {}
+    
+	
+	public void onLivingUpdate() {
+		if (!this.worldObj.isRemote)
+        {
+            --this.risingTimer;
+
+            if (this.risingTimer <= 0)
+            {
+                this.risingTimer = this.rand.nextInt(40)+20;
+                this.rising = !this.rising;
+            }
+
+            if (this.getEntityToAttack() == null && this.rising)
+            {
+                this.motionY += (0.30000001192092896D - this.motionY) * 0.30000001192092896D;
+            }
+            else if (this.getEntityToAttack() != null && this.getEntityToAttack().posY + (double)this.getEntityToAttack().getEyeHeight() > this.posY + (double)this.getEyeHeight())
+            {
+            	this.motionY += (0.30000001192092896D - this.motionY) * 0.30000001192092896D;
+            }
+        }
+        if (!this.onGround && this.motionY < 0.0D)
+        {
+            this.motionY *= 0.6D;
+        }
+		super.onLivingUpdate();
+	}
 }
